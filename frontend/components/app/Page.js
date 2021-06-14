00b3a1aec716bf158819e025f03b33dc8594735f
shaunpearce <shaun@comediadesign.com>
2021-06-14 15:58:10 +0200

    Adding mobile message

--- frontend/components/app/Page.js
+++ frontend/components/app/Page.js
@@ -1,5 +1,13 @@
 const Page = ({ ...props }) => {
-  return <main {...props} />
+  return (
+    <>
+      {" "}
+      <main {...props} />
+      <div className="z-30 fixed left-0 top-0 h-full w-full items-center justify-center p-8 flex bg-white bg-opacity-100 lg:hidden">
+        <h3>This page is not mobile optimized yet!</h3>
+      </div>
+    </>
+  )
 }
 
 export default Page
