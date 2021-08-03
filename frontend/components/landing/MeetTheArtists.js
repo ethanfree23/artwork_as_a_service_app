a36245b33ea885202d9aaafb984bf4a781c02b23
shaunpearce <shaun@comediadesign.com>
2021-08-03 15:28:05 +0200

    Fixing grid

--- frontend/components/landing/MeetTheArtists.js
+++ frontend/components/landing/MeetTheArtists.js
@@ -14,8 +14,8 @@ const MeetTheArtists = ({ artists }) => {
       <div className="grid gap-10 grid-cols-3 h-96">
         {artists?.map((artist, index) => (
           // <Link key={index} href={`/gallery/${artist.id}`}>
-          <div key={index}>
-            <div className="grid grid-cols-2 gap-2 overflow-hidden cursor-pointer">
+          <div key={index} className="flex">
+            <div className="grid grid-cols-2 gap-2 overflow-hidden cursor-pointer w-full">
               <Video className="flex-1" video={artist.video} />
               <div className="flex-1 flex flex-col gap-2 overflow-hidden">
                 {artist?.arts?.slice(0, 3)?.map((art, index) => (
